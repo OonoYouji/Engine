823603d138d3638714ea9c00b4d8638e60ef99c6
OonoYouji <k023g0007@g.neec.ac.jp>
2024-01-19 23:20:33 +0900

    windowを表示

--- Engine/WinApp.cpp
+++ Engine/WinApp.cpp
@@ -1,1 +1,8 @@
 #include "WinApp.h"
+
+
+
+WinApp* WinApp::GetInstance() {
+	static WinApp instance;
+	return &instance;
+}
--- Engine/WinApp.h
+++ Engine/WinApp.h
@@ -1,6 +1,26 @@
 #pragma once
 
-class WinApp {
+#include <Windows.h>
+
+class WinApp final {
+private:
+
+	WinApp() = default;
+	~WinApp() = default;
+
+	/// 代入演算子、コピーコンストラクタの禁止;
+	WinApp(const WinApp& other) = delete;
+	WinApp(WinApp&& other) = delete;
+	const WinApp& operator= (const WinApp& other) = delete;
+
+public:
+
+	/// <summary>
+	/// インスタンス確保
+	/// </summary>
+	/// <returns>WinAppへのポインタ</returns>
+	static WinApp* GetInstance();
+
 
 
 
